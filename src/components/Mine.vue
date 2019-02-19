40756fe3a00468abff0b0d7241009ee6aeb4ecee
liguangyuan <liguangyuan@lechebang.com>
2019-02-19 18:13:58 +0800

    显示

--- src/components/Mine.vue
+++ src/components/Mine.vue
@@ -1,8 +1,11 @@
 <template>
   <div :style="{width:allObj.width,height:allObj.height}">
-        <baidu-map class="map" center="上海">
-          <bm-geolocation anchor="BMAP_ANCHOR_BOTTOM_RIGHT" :showAddressBar="true" :autoLocation="true"></bm-geolocation>
-        </baidu-map>
+      <baidu-map class="map" center="上海">
+        <bm-geolocation anchor="BMAP_ANCHOR_BOTTOM_RIGHT" :showAddressBar="true" :autoLocation="true"></bm-geolocation>
+      </baidu-map>
+      <div class="p_right" @click="subPet">
+          <span class="subText">发布</span>
+      </div>
   </div>
 </template>
 <script>
@@ -16,12 +19,16 @@ export default {
         width: 0,
         height: 0
       },
+      value1: '',
       newWidth: {
         
       }
     };
   },
   methods: {
+    subPet: function(){
+      this.$router.push('/Message');
+    }
   },
   mounted(){
   },
@@ -34,6 +41,21 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.subText{
+  font-size: 15px;
+  font-weight: 500;
+}
+.p_right{
+  width: 50px;
+  height: 50px;
+  background-color: aqua;
+  position: absolute;
+  top:100px;
+  right: 30px;
+  border-radius: 25px;
+  text-align: center;
+  line-height: 50px;
+}
 h1{
   color:rgba(228, 41, 135, 0.575);
 }
--- src/main.js
+++ src/main.js
@@ -5,11 +5,14 @@ import 'mint-ui/lib/style.css';
 import axios from 'axios';
 import router from './router';
 import BaiduMap from 'vue-baidu-map';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(BaiduMap, {
   ak: '1qo1ZrgDs2F7fxV7LbQGaF4k0TCcPq3g'
 });
 Vue.use(MintUI);
+Vue.use(ElementUI, { size: 'small', zIndex: 3000 });
 Vue.config.productionTip = false;
 Vue.prototype.$ajax = axios;
 
